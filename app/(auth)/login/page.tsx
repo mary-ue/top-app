37044c1f04f04b26fdc0af76ce89532b5098497b
mary-ue <maryandreeva100@gmail.com>
2024-08-01 18:19:00 +0300

    structure of project

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About',
+};
+
+export default function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
