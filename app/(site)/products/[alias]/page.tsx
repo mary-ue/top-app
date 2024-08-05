94bf6e998ff9fab4985ffc20e5033ceef0a291f7
mary-ue <maryandreeva100@gmail.com>
2024-08-05 18:57:43 +0300

    generateStaticParams

--- app/(site)/products/[alias]/page.tsx
+++ app/(site)/products/[alias]/page.tsx
@@ -1,3 +1,4 @@
+import { getMenu } from '@/api/menu';
 import { getPage } from '@/api/page';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
@@ -5,6 +6,11 @@ import { notFound } from 'next/navigation';
 export const metadata: Metadata = {
   title: 'Страница',
 };
+
+export async function generateStaticParams() {
+  const menu = await getMenu(0);
+  return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias})))
+}
 
 export default async function PageProducs({
   params,
