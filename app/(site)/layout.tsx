764782b2dbadc11d476d3e12e1cb7dc2f59d09cc
mary-ue <maryandreeva100@gmail.com>
2024-08-29 00:54:49 +0300

    font

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Open_Sans } from 'next/font/google';
 import './globals.css';
 import styles from './page.module.css';
 import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
